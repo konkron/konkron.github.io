3767b853b0b2b6aa911c13750680b953775c841c
konkron <trygve.lokkevik@gmail.com>
2016-05-12 20:54:03 +0200

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -1,6 +1,7 @@
 $email=$_POST['email'];
-$message=json_encode($_POST);
+$message=$_POST['title'];
+$message=$_POST['message'];
 $receiver="kontakt@konkron.com";
 $mailer=$_POST['name'];
 
-mail($email,"",$message,  array("from"=>$mailer));
+mail($email,$title,$message,  array("from"=>$mailer));
