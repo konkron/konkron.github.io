1094d00b257a065a70c3681e83517c61377c3441
konkron <trygve.lokkevik@gmail.com>
2016-05-12 21:55:56 +0200

    Delete sendmail.php

--- sendmail.php
+++
@@ -1,7 +0,0 @@
-$email=$_POST['email'];
-$message=$_POST['title'];
-$message=$_POST['message'];
-$receiver="kontakt@konkron.com";
-$mailer=$_POST['name'];
-
-mail($email,$title,$message,  array("from"=>$mailer));
